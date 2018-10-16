2a3e3d56851d7a8a93ac1fa64778428a5d2366f5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:14:56 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,5 +1,5 @@
 
 library(ggplot2)
-histDay <- ggplot(raw_data, aes(x=Day.of.Month))
-histDay <- histDay + geom_histogram(binwidth=1, color="black", fill=Satisfaction)
+histDay <- ggplot(raw_data, aes(x=Day.of.Month, Satisfaction))
+histDay <- histDay + geom_histogram(binwidth=1, color="black", fill="white")
 histDay <- histDay + ggtitle("Day of Month Histogram")
