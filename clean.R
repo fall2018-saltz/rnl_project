9a4a205c39bd57b55ec3a0b7d5d1a754f4ab6bff
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:09:37 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,5 @@
 
-clean_data <- raw_data
+library(ggplot2)
+histDay <- ggplot(raw_data, aes(x=Population))
+histDay <- histPop + geom_histogram(binwidth=5000000, color="black", fill="white")
+histDay <- histPop + ggtitle("State Population Histogram")
