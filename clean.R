a5c446a194839905fdac0b6222ee77250ebeb6e8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:23:15 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,6 +1,6 @@
 
 library(ggplot2)
-barDay <- ggplot(raw_data, aes(x=Day.of.Month, y=Satisfaction, group = 1, fill = Gender)) + geom_col() + 
+barDay <- ggplot(raw_data, aes(x=Day.of.Month, y=Satisfaction, group = 1)) + geom_col() + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1), axis.text.y = element_text(angle = 90, hjust = 1)) + 
   ggtitle('Day of Month')
 
