fca6c3e547409550291de52fa85f82ef1200ff5c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:19:26 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,5 +1,5 @@
 
 library(ggplot2)
-barDay <- ggplot(raw_data, aes(x=reorder(stateName, TotalMurders), y=TotalMurders, group = 1, fill = percentOver18)) + geom_col() + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle('Murders Per State - Colored by Percent Over 18')
+barDay <- ggplot(raw_data, aes(x=Day.of.Month, y=Satisfaction, group = 1, fill = Gender)) + geom_col() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle('Day of Month')
 
