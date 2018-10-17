7f1d3b130aeb3fb38238c9ccaf58e57daf1b8eb0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:09:34 -0400

    Saving summarize.R

--- summarize.R
+++ summarize.R
@@ -7,6 +7,6 @@ SatisfactionByDay <- summarize(df.GroupByDays, Satisfaction = sum(Satisfaction>=
 
 tapply <- tapply(raw_data$Satisfaction>=0, raw_data$Day.of.Month, sum, na.rm = TRUE)
 
-tapply <- tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, mean, na.rm = TRUE)
-tapply <- tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, median, na.rm = TRUE)
-tapply <- tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, sd, na.rm = TRUE)
+mean <- tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, mean, na.rm = TRUE)
+median <- tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, median, na.rm = TRUE)
+sd <- tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, sd, na.rm = TRUE)
