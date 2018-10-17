04938c8cf9f7ce3d57cd3d52b44d0da8a474fb0f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:52:53 -0400

    Saving summarize.R

--- summarize.R
+++ summarize.R
@@ -3,3 +3,6 @@ DayofMonth <- raw_data[,c(1,14)]
 library(dplyr)
 df.GroupByDays <- group_by(DayofMonth, Day.of.Month)
 SatisfactionByDay <- summarize(df.GroupByDays, Satisfaction = sum(Satisfaction>=0, na.rm = TRUE))
+
+
+tapply <- tapply(raw_data$Satisfaction, raw_data$Day.of.Month, mean, na.rm = TRUE)
