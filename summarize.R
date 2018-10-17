4b4a568147d81f2ace82895adab44affff83b5d3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 12:55:32 -0400

    Saving summarize.R

--- summarize.R
+++ summarize.R
@@ -5,4 +5,4 @@ df.GroupByDays <- group_by(DayofMonth, Day.of.Month)
 SatisfactionByDay <- summarize(df.GroupByDays, Satisfaction = sum(Satisfaction>=0, na.rm = TRUE))
 
 
-tapply <- tapply(raw_data$Satisfaction, raw_data$Day.of.Month, sum, na.rm = TRUE)
+tapply <- tapply(raw_data$Satisfaction>=0, raw_data$Day.of.Month, sum, na.rm = TRUE)
