431cfcd20fc4bbfa6ef36c3f467822d873f31918
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:17:22 -0400

    Saving summarize.R

--- summarize.R
+++ summarize.R
@@ -12,7 +12,7 @@ mean <- tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, mea
 median <- tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, median, na.rm = TRUE)
 sd <- tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, sd, na.rm = TRUE)
 
-#barMean <- ggplot(mean, aes(x=Day.of.Month, y=Satisfaction, group = 1)) + geom_col() 
+barMean <- ggplot(mean, aes(x=Day.of.Month, y=Satisfaction, group = 1)) + geom_col() 
 
 histMean <- hist(mean)
 histMedian <- hist(median)
