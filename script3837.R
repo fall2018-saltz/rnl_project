ce11a03b509e8d759cdc3656ff2f2c9d8c039a35
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 14:12:28 -0400

    Saving script3837.R

--- script3837.R
+++ script3837.R
@@ -4,7 +4,7 @@ library(ggplot2)
 DayofMonth <- raw_data[,c(1,14)]
 library(dplyr)
 
-mean <- data.frame(tapply(as.numeric(DayofMonth$Satisfaction), as.numeric(na.omit(DayofMonth$Day.of.Month)), mean, na.rm = TRUE))
+mean <- data.frame(tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, mean, na.rm = TRUE))
 colnames(mean) <- "Satisfaction"
 mean$Day.of.Month <- rownames(mean)
 barMean <- ggplot(mean, aes(x=Day.of.Month, y=Satisfaction, group = 1)) + geom_col() 
