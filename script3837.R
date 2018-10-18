6bcdffc86e8351fcc964bbab8c008aaa407a296b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 14:13:18 -0400

    Saving script3837.R

--- script3837.R
+++ script3837.R
@@ -1,8 +1,7 @@
 
-
+library(dplyr)
 library(ggplot2)
 DayofMonth <- raw_data[,c(1,14)]
-library(dplyr)
 
 mean <- data.frame(tapply(as.numeric(DayofMonth$Satisfaction), DayofMonth$Day.of.Month, mean, na.rm = TRUE))
 colnames(mean) <- "Satisfaction"
