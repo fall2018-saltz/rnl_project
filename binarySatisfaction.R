9064fa4fcb70380efc3a693ad74a6e1bcc2de075
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 16:56:13 -0400

    Saving binarySatisfaction.R

--- binarySatisfaction.R
+++ binarySatisfaction.R
@@ -1,2 +1,4 @@
 
-summary <- summary(raw_data)
+cleanSatisfactionSurvey <- satisfactionSurvey[(satisfactionSurvey$Satisfaction!="4.00.2.00"&satisfactionSurvey$Satisfaction!="4.00.5"),]
+cleanSatisfactionSurvey$SatisfactionNumeric <- as.numeric(as.character(cleanSatisfactionSurvey$Satisfaction))
+cleanSatisfactionSurvey$binarySat <- cleanSatisfactionSurvey$SatisfactionNumeric > 3.5
